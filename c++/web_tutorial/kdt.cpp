702f2645d024b50763fe992fddc55ecd7c5373e5
krs <krs@uncc.edu>
2018-12-22 15:28:40 -0500

    kdt example update

---
+++ c++/web_tutorial/kdt.cpp
@@ -0,0 +1,49 @@
+#include "Bridges.h"
+#include "KdTreeElement.h"
+
+using namespace bridges;
+
+int main(int argc, char **argv) {
+	if (argc < 3) {
+		cout << "Must provide both user id and api key as command line arguments. Terminating.." 
+								<< endl;
+		exit (-1);
+	}	
+	Bridges *bridges = new Bridges(15, argv[1], argv[2]);
+	
+	if (argc == 4)		// Server type provided
+		bridges->setServer(argv[3]);
+		
+	bridges->setTitle("A Kd Tree Example");
+
+
+	KdTreeElement<int, int> *t0 = new KdTreeElement<int, int>(50, 0);
+	KdTreeElement<int, int> *t1 = new KdTreeElement<int, int>(25, 1);
+	KdTreeElement<int, int> *t2 = new KdTreeElement<int, int>(75, 1);
+	KdTreeElement<int, int> *t3 = new KdTreeElement<int, int>(20, 0);
+	KdTreeElement<int, int> *t4 = new KdTreeElement<int, int>(30, 0);
+	KdTreeElement<int, int> *t5 = new KdTreeElement<int, int>(60, 0);
+	KdTreeElement<int, int> *t6 = new KdTreeElement<int, int>(80, 0);
+
+	t0->setLeft(t1);
+	t0->setRight(t2);
+	t1->setLeft(t3);
+	t1->setRight(t4);
+	t2->setLeft(t5);
+	t2->setRight(t6);
+
+	t0->getVisualizer()->setColor(Color("red"));
+	// color the leaf nodes that represent the code letters
+	t1->getVisualizer()->setColor(Color("orange"));
+	t4->getVisualizer()->setColor(Color("orange"));
+	t5->getVisualizer()->setColor(Color("orange"));
+	t6->getVisualizer()->setColor(Color("orange"));
+
+	// provide BRIDGES the  handle to the tree structure
+	bridges->setDataStructure(t0);
+
+	bridges->setVisualizeJSONFlag(true);
+	bridges->visualize();
+
+	return 0;
+}
