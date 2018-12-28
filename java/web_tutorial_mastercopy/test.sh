a2aeecd4d1f1de1c51d95718e0c6e63e18fbab5e
Erik Saule <esaule@uncc.edu>
2018-12-28 13:21:29 -0500

    generate web and testing copy using a makefile

--- java/web_tutorial_mastercopy/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-for file in *.java;
-do
-    unifdef -DTESTING=1  ${file} -o ../web_tutorial_testing/${file}
-done
-
--- java/web_tutorial_mastercopy/to_srcs.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-for file in *.java
-do
-    unifdef -DTESTING=0 ${file} -o ../web_tutorial_fortheweb/${file}
-done
