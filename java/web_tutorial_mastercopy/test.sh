408c70ad33cd9a06e9892a70b7b7237ccd3b089c
Erik Saule <esaule@uncc.edu>
2018-12-28 12:45:57 -0500

    differentiating scripts

--- java/web_tutorial_mastercopy/test.sh
+++ java/web_tutorial_mastercopy/test.sh
@@ -5,5 +5,3 @@ do
     unifdef -DTESTING=1  ${file} -o ../web_tutorial_testing/${file}
 done
 
-# unifdef -DTESTING=0  bst.java > test.java
-# mv test.java tmp/bst.java
--- java/web_tutorial_mastercopy/to_srcs.sh
+++ java/web_tutorial_mastercopy/to_srcs.sh
@@ -1,13 +1,6 @@
 #!/bin/sh
 
-TESTING_MODE=1
-
 for file in *.java
 do
-	if test "${TESTING_MODE}" -eq 0 ; then
-		unifdef -DTESTING=0 ${file} > ../web_tutorial_fortheweb/${file}
-	else
-		unifdef -DTESTING=1 ${file} > ../web_tutorial/${file}
-	fi
-
+    unifdef -DTESTING=0 ${file} -o ../web_tutorial_fortheweb/${file}
 done
