95854eb3879e62d751948e952bb72ab4959f8f9e
Erik Saule <esaule@uncc.edu>
2018-12-27 20:07:26 -0500

    script to generate testing code

--- java/web_tutorial_mastercopy/test.sh
+++ java/web_tutorial_mastercopy/test.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
-unifdef -DTESTING=0  bst.java > test.java
-mv test.java tmp/bst.java
+for file in *.java;
+do
+    unifdef -DTESTING=1  ${file} -o ../web_tutorial/${file}
+done
+
+# unifdef -DTESTING=0  bst.java > test.java
+# mv test.java tmp/bst.java
