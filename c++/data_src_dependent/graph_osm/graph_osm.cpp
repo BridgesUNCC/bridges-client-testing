0cc0c1c0461a88fd6ad3e48f49f539cdc9680299
Erik Saule <esaule@uncc.edu>
2019-02-17 00:05:09 -0500

    more robust version of the code

--- c++/data_src_dependent/graph_osm/graph_osm.cpp
+++ c++/data_src_dependent/graph_osm/graph_osm.cpp
@@ -26,15 +26,22 @@ void latlongToCartesianCoords (double lat, double longit, double *cartesian_coor
 //	cartesian_coords[1] = R * sin(lat_rad);
 }
 
-const int num_vertices = 1885;
-const int num_edges = 2647;
-string vertex_filename = "vertices.txt";
-string edges_filename = "edges.txt";
 
 int main(int argc, char **argv) {
 
-	//create the Bridges object, set credentials
-    Bridges *bridges =  new Bridges(12, "kalpathi60", "486749122386");
+  //const int num_vertices = 1885;
+  //const int num_edges = 2647;
+  string vertex_filename = "vertices.txt";
+  string edges_filename = "edges.txt";
+  
+  if (argc > 2) {
+    vertex_filename = argv[1];
+    edges_filename = argv[2];
+    
+  }
+  
+  //create the Bridges object, set credentials
+  Bridges *bridges =  new Bridges(16, "esaule", "SOMETHING");
 
 	
 	bridges->setTitle("Graph : OpenStreet Map Example");
@@ -51,26 +58,30 @@ int main(int argc, char **argv) {
 			longit_range[] = {10000., -10000.};
 	double cartesian_coords[2];
 	cout << "long is " << sizeof(long) << " bytes.."  << endl;
-	for (int k = 0; k < num_vertices; k++) {
+	int num_vertices = 0;
+	while (1) {
 		vert_file >> vert_id >> lat >> longit; 	
-
+		if (!(vert_file.good()))
+		  break;
+		
 		cartesian_coords[0] = lat; cartesian_coords[1] = longit;
 								// convert to cartesian coords
 		latlongToCartesianCoords (lat, longit, cartesian_coords);
 		
 								// remap vertex ids to 0..maxVertices
-		vert_map[vert_id] = k;
+		vert_map[vert_id] = num_vertices;
 								// compute lat/long range
 		lat_range[0]  = std::fmin(cartesian_coords[0], lat_range[0]); 
 		lat_range[1]  = std::fmax(cartesian_coords[0], lat_range[1]); 
 		longit_range[0]  = std::fmin(cartesian_coords[1], longit_range[0]); 
 		longit_range[1]  = std::fmax(cartesian_coords[1], longit_range[1]);
 								// add vertext to graph
-		graph.addVertex(k, 0);
+		graph.addVertex(num_vertices, 0);
 								// set its location
-		graph.getVertex(k)->getVisualizer()->setLocation(cartesian_coords[0], cartesian_coords[1]);
-//		graph.getVertex(k)->getVisualizer()->setSize(1.0f);
-		graph.getVertex(k)->getVisualizer()->setColor(Color("green"));
+		graph.getVertex(num_vertices)->getVisualizer()->setLocation(cartesian_coords[0], cartesian_coords[1]);
+//		graph.getVertex(num_vertices)->getVisualizer()->setSize(1.0f);
+		graph.getVertex(num_vertices)->getVisualizer()->setColor(Color("green"));
+		num_vertices++;
 	}
 	cout << "Range: Lat: " << lat_range[0] << "," << lat_range[1] << endl;
 	cout << "Range: Longit: " << longit_range[0] << "," << longit_range[1] << endl;
@@ -105,8 +116,12 @@ int main(int argc, char **argv) {
 	long v1, v2; 
 	double dist;
 
-	for (int k = 0; k < num_edges; k++) {
+	int num_edges = 0;
+	while (1) {
+	//for (int k = 0; k < num_edges; k++) {
 		edges_file >> v1 >> v2 >> dist;
+		if (!(edges_file.good()))
+		  break;
 				// check if vertices exist
 		if (vert_map.find(v1) == vert_map.end() || vert_map.find(v2) == vert_map.end()) {
 			cout << "Vertex not found!! Exiting.." << endl;
@@ -115,7 +130,8 @@ int main(int argc, char **argv) {
 		int v1a = vert_map[v1], v2a = vert_map[v2];
 		graph.addEdge (v1a, v2a, int(dist));
 		graph.getLinkVisualizer(v1a, v2a)->setColor(Color("orange"));	
-		graph.getLinkVisualizer(v1a, v2a)->setThickness(5.0f);	
+		graph.getLinkVisualizer(v1a, v2a)->setThickness(5.0f);
+		num_edges++;
 	}
 
 	bridges->visualize();
