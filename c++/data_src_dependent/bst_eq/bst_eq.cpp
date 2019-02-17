4666a0ab835ac838112bf952d02051d8d7efbd63
Erik Saule <esaule@uncc.edu>
2019-02-17 13:18:09 -0500

    why so many pointers?

--- c++/data_src_dependent/bst_eq/bst_eq.cpp
+++ c++/data_src_dependent/bst_eq/bst_eq.cpp
@@ -22,29 +22,29 @@ int main(int argc, char **argv) {
 		
 #if TESTING
                         // command line args provide credentials and server to test on
-    Bridges *bridges =  new Bridges(atoi(argv[1]), argv[2], argv[3]);
+    Bridges bridges (atoi(argv[1]), argv[2], argv[3]);
     if (argc > 4)
-        bridges->setServer(argv[4]);
+        bridges.setServer(argv[4]);
 #else
-    Bridges *bridges =  new Bridges(YOUR_ASSSIGNMENT_NUMBER, "YOUR_USER_ID", 
+    Bridges bridges (YOUR_ASSSIGNMENT_NUMBER, "YOUR_USER_ID", 
                                 "YOUR_API_KEY");
 #endif
 								// read the earth quake  data and build the BST
-	bridges->setTitle("Recent Earthquakes (USGIS Data)");
+	bridges.setTitle("Recent Earthquakes (USGIS Data)");
 
-	DataSource *ds = new DataSource;
-    vector<EarthquakeUSGS> eq_list = ds->getEarthquakeUSGSData(max_quakes);
+	DataSource ds (&bridges);
+    vector<EarthquakeUSGS> eq_list = ds.getEarthquakeUSGSData(max_quakes);
 
-	BST<float, EarthquakeUSGS> *bst = new BST<float, EarthquakeUSGS> ();
+    BST<float, EarthquakeUSGS> bst;
 
 	for (int k = 0; k < max_quakes; k++) {
 		EarthquakeUSGS eq = eq_list[k];
-		bst->insert(eq.getMagnitude(), eq);
+		bst.insert(eq.getMagnitude(), eq);
 	}
 
 					// visualize the binary search tree
-	bridges->setDataStructure(bst->getRoot());
-	bridges->visualize();
+	bridges.setDataStructure(bst.getRoot());
+	bridges.visualize();
 
 	return 0;
 }
