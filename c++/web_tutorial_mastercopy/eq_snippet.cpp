ab4c01f5ae378e488bf2835742b9758a26ac848d
Kalpathi Subramanian <krs@uncc.edu>
2019-07-12 19:46:06 -0400

    eq_snippet - not finished

---
+++ c++/web_tutorial_mastercopy/eq_snippet.cpp
@@ -0,0 +1,36 @@
+
+#include <string>
+
+using namespace std;
+
+#include "Bridges.h"
+#include "DataSource.h"
+#include "data_src/EarthquakeUSGS.h"
+
+
+using namespace bridges;
+
+int max_quakes = 100;
+
+int main(int argc, char **argv) {
+
+#if TESTING
+	// command line args provide credentials and server to test on
+	Bridges bridges (atoi(argv[1]), argv[2], argv[3]);
+	if (argc > 4)
+		bridges.setServer(argv[4]);
+#else
+	Bridges bridges (YOUR_ASSSIGNMENT_NUMBER, "YOUR_USER_ID",
+		"YOUR_API_KEY");
+#endif
+	// read the earth quake  data and build the BST
+	bridges.setTitle("Accessing USGIS Earthquake Data (USGIS Data)");
+
+	DataSource ds (&bridges);
+	vector<EarthquakeUSGS> eq_list = ds.getEarthquakeUSGSData(max_quakes);
+
+	cout << "Earthquake 0:" << endl;
+	cout << "\tMagnitude:" << eq_list[0].getMagnitude() << endl;
+
+	return 0;
+}
---
+++ c++/web_tutorial_mastercopy/game_snippet.cpp
@@ -0,0 +1,44 @@
+#include <string>
+
+using namespace std;
+
+#include "Bridges.h"
+#include "DataSource.h"
+#include "data_src/Game.h"
+
+
+using namespace bridges;
+
+
+int main(int argc, char **argv) {
+	string hilite_color = "orange", 
+			def_color = "green",
+			end_color = "red";
+#if TESTING
+                        // command line args provide credentials and server to test on
+    Bridges bridges (atoi(argv[1]), argv[2], argv[3]);
+    if (argc > 4)
+        bridges.setServer(argv[4]);
+#else
+    Bridges bridges (YOUR_ASSSIGNMENT_NUMBER, "YOUR_USER_ID", 
+                                "YOUR_API_KEY");
+#endif
+		
+	bridges.setTitle("How to access the IGN Game Data");
+								// read the data 
+
+	DataSource ds (&bridges);
+    vector<Game> game_list = ds.getGameData();
+
+					// Print a single record of the data for illustration
+	
+	cout << "Game 0:" << endl;
+	cout << "\tTitle: "<< game_list[0].getTitle() << endl 
+		<< "\tPlatform Type: " << game_list[0].getPlatformType() << endl
+		<< "\tRating: " << game_list[0].getRating() << endl << 
+		"\tGenres: "; 
+	for (auto& s : game_list[0].getGameGenre())
+		cout << s << "," << endl;
+
+	return 0;
+}
