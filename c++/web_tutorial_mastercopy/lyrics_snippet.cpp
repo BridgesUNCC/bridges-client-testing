3679478908d7cac6c9c345f2dbfccce76701ec4f
Kalpathi Subramanian <kalpathi@Kalpathis-Mac-mini.local>
2019-07-13 16:36:36 -0400

    lyrics_snippet

---
+++ c++/web_tutorial_mastercopy/lyrics_snippet.cpp
@@ -0,0 +1,43 @@
+#include "Bridges.h"
+#include "DataSource.h"
+#include "Array.h"
+#include "SLelement.h"
+#include "ColorGrid.h"
+#include <sstream>
+#include <string>
+
+using namespace bridges;
+
+int main(int argc, char* argv[]) {
+
+	//create the Bridges object, set credentials
+#if TESTING
+	// command line args provide credentials and server to test on
+	Bridges bridges(atoi(argv[1]), argv[2], argv[3]);
+	if (argc > 4)
+		bridges.setServer(argv[4]);
+#else
+	Bridges bridges(YOUR_ASSSIGNMENT_NUMBER, "YOUR_USER_ID",
+		"YOUR_API_KEY");
+#endif
+
+	bridges.setTitle("Accessing Song Data");
+
+	// create data source object
+	DataSource ds (&bridges);
+
+	//Get Song data 
+	Song s = ds.getSong("Harder Faster Better Stronger", "Daft Punk");
+
+	//print lyrics
+	std::cout<<s.getLyrics()<<std::endl;
+
+
+	//print lyrics one word at a time
+	//	for (auto w : words) {
+	//	  std::cout<<w<<std::endl;
+	//	}
+
+
+	return 0;
+}
