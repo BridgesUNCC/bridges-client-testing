6d67ffd99c2f179f8a9fa80c6629777cfad9d586
Kalpathi Subramanian <krs@uncc.edu>
2019-07-29 20:32:30 -0400

    array fixes

--- c++/web_tutorial_mastercopy/array3d.cpp
+++ c++/web_tutorial_mastercopy/array3d.cpp
@@ -21,19 +21,32 @@ int main(int argc, char **argv) {
 	bridges.setDescription("A 3 dimensional array with visual attributes. The third dimension is illustrated as a sequence of 2D slices");
 
 		
+	// specify array dimensions and create 3D array
+	int num_slices = 4, num_rows = 4, num_cols = 4;
+
 	// create, populate 3D array
 	Array3D<int> my_array (4, 4, 4);
 
+	// populate the elements with their array position
+	int n  = 0;
+	for (int k = 0; k < num_slices; k++) 
+		for (int j = 0; j < num_rows; j++) 
+			for (int i = 0; i < num_cols; i++) {
+				my_array.getElement(k, j, i).setLabel("El " + std::to_string(n));
+				n++;
+			}
+
+
 	// color some of the elements
-	my_array[0, 0, 0].setColor("red");
-	my_array[0, 3, 0].setColor("green");
-	my_array[3, 0, 0].setColor("blue");
-	my_array[3, 3, 0].setColor("magenta");
-	my_array[1, 1, 0].setColor("cyan");
-	my_array[2, 2, 0].setColor("yellow");
+	my_array.getElement(0, 0, 0).setColor("red");
+	my_array.getElement(0, 3, 0).setColor("green");
+	my_array.getElement(1, 1, 0).setColor("cyan");
+	my_array.getElement(2, 2, 0).setColor("yellow");
+	my_array.getElement(3, 0, 0).setColor("blue");
+	my_array.getElement(3, 3, 3).setColor("magenta");
 
 	// visualize
-	bridges.setDataStructure(&arr);
+	bridges.setDataStructure(my_array);
 	bridges.visualize();
 
 	return 0;
