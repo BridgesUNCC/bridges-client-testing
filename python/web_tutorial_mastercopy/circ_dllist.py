00541abd2e14da539d4aa092b72a49438faabef1
Kalpathi Subramanian <krs@uncc.edu>
2019-07-31 13:03:18 -0400

    python test updates

--- python/web_tutorial_mastercopy/circ_dllist.py
+++ python/web_tutorial_mastercopy/circ_dllist.py
@@ -3,11 +3,10 @@ from bridges.circ_dl_element import *
 import sys
 
 def main():
-    args = sys.argv[1:]
-
     # create the Bridges object, set credentials
 #if TESTING
     # command line args provide credentials and server to test on
+    args = sys.argv[1:]
     bridges = Bridges(int(args[0]), args[1], args[2])
     if len(args) > 3:
         bridges.connector.set_server(args[3])
