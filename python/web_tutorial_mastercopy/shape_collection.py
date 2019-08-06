0fd8ebcfe7f96310f1ffcea6b0e1e0e850899ba2
Kalpathi Subramanian <krs@uncc.edu>
2019-08-06 10:29:08 -0400

    adding gettime to eq tutorial

--- python/web_tutorial_mastercopy/shape_collection.py
+++ python/web_tutorial_mastercopy/shape_collection.py
@@ -26,23 +26,27 @@ def main():
     bridges.set_description("Red square, green circle, magenta horizontal and vertical lines, "
             +	"and a test label with a purple outline.")
 
+    # draw a rectangle
     sc = SymbolCollection()
     s1 = Rectangle(w=50, h=50, locx=-25.0, locy=0.0)
     s1.fill_color = "red"
     sc.add_symbol(s1)
 
+    # draw a rectangle
     s2 = Circle(locx=25.0, locy=0.0, r=25)
     s2.fill_color = "green"
     sc.add_symbol(s2)
 
+    # draw a triangle
     s3 = Polygon()
     s3.add_point(-25, -10)
-    s3.add_point(25, 0)
+    s3.add_point(25, -10)
     s3.add_point(0.0, 50.0)
     s3.stroke_color = "darkorchid"
     s3.stroke_width = 2.0
     sc.add_symbol(s3)
 
+    # draw X axis
     s4 = Polyline()
     s4.add_point(-100, 0)
     s4.add_point(100, 0)
@@ -50,12 +54,14 @@ def main():
     s4.stroke_width = 2.0
     sc.add_symbol(s4)
 
+    # draw Y axis
     s5 = Polyline()
     s5.add_point(0.0, -100.0)
     s5.add_point(0.0, 100.0)
     s5.stroke_width = 2.0
     sc.add_symbol(s5)
 
+    # draw another triangle
     s6 = Polygon()
     s6.add_point(-30.0, 40.0)
     s6.add_point(30.0, 40.0)
