4dfceaff33a3b6b611d8173d3fbdc47d2e8010e1
Erik Saule <esaule@uncc.edu>
2020-03-02 16:00:18 -0500

    formatting

--- python/web_tutorial_mastercopy/elevation.py
+++ python/web_tutorial_mastercopy/elevation.py
@@ -19,13 +19,12 @@ def main():
 
 
     ele_obj = data_source.get_elevation_data([-98.02593749997456, 41.03133177632377, -96.94531249997696, 42.508577297430456])
-    print("width: "+str(ele_obj.cols))
-    print("height: "+str(ele_obj.rows))
-    print(ele_obj._xll)
-    print(ele_obj._yll)
-    print("spatial resolution: " + ele_obj.cellsize)
+    print("Width: "+str(ele_obj.cols))
+    print("Height: "+str(ele_obj.rows))
+    print("Coordinate of lower left corner: "+str(ele_obj.xll)+", "+str(ele_obj.yll))
+    print("Spatial resolution: " + ele_obj.cellsize)
 
-    print("first 10 elevations:")
+    print("First 10 elevations:")
     for i in range(0, 10):
         print(ele_obj.data[0][i])
     
