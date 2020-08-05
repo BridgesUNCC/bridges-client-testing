cf4d03ffdfd7f58b31838c74ce04c9ee7d8bf9b9
Kalpathi Subramanian <krs@uncc.edu>
2020-08-05 16:51:43 -0400

    game_size test added

---
+++ python/web_tutorial_mastercopy/game_size_test.py
@@ -0,0 +1,31 @@
+from bridges import *
+
+class GameSizeTest (NonBlockingGame): 
+	def	 __init__(self, assid, user_id, api_key):
+		# substitute your username and api key
+		# create a game board of 10x10
+		#self.set_title ("Game tutorial 2  - drawing symbols")
+		super().__init__(assid, user_id,  api_key, 10, 20)
+
+	def	initialize(self):
+		# this gets executed once
+		for i in range(10):
+			for j in range(10):
+				self.set_bg_color(i, j, NamedColor.ivory)
+
+		self.draw_symbol(0, 0, NamedSymbol.H, NamedColor.red)
+		self.draw_symbol(0, 19, NamedSymbol.E, NamedColor.green)
+		self.draw_symbol(9, 0, NamedSymbol.L, NamedColor.blue)
+		self.draw_symbol(9, 19, NamedSymbol.O, NamedColor.cyan)
+
+	def	game_loop(self): 
+		# this gets executed every frame
+		pass
+
+def	main():
+	game = GameSizeTest(int(args[0], args[1], args[2]))
+	# start the game
+	game.start()
+
+if __name__ == "__main__":
+	main()
