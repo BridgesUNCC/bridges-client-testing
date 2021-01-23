fa2800fb58a83a15db226f946f245d5260888125
Erik Saule <esaule@uncc.edu>
2021-01-23 16:38:40 -0500

    removing dynamic allocation in graph_alist

--- c++/web_tutorial_mastercopy/graph_alist.cpp
+++ c++/web_tutorial_mastercopy/graph_alist.cpp
@@ -26,8 +26,8 @@ int main(int argc, char **argv) {
 #endif
 
 	// get the actor - movie data from IMDB dataset
-	DataSource *ds = new DataSource;
-	vector<ActorMovieIMDB> actor_list = ds->getActorMovieIMDBData(1813);
+	DataSource ds (bridges);
+	vector<ActorMovieIMDB> actor_list = ds.getActorMovieIMDBData(1813);
 
 	// create a graph
 	GraphAdjList<string, string, int> graph;
