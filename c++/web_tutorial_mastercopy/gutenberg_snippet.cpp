cf69e4a8a8a0fd3004bda1a47db2ef8006506d48
Kalpathi Subramanian <krs@uncc.edu>
2021-06-08 21:49:09 -0400

    gutenberg test working now..

--- c++/web_tutorial_mastercopy/gutenberg_snippet.cpp
+++ c++/web_tutorial_mastercopy/gutenberg_snippet.cpp
@@ -32,27 +32,32 @@ int main(int argc, char **argv) {
 	GutenbergBook gbook = ds.getGutenbergBookMetaData(2701);
 
 	// print the first book record
-	cout << "Guttenberg Book Data:\n\n"; 
+	cout << "Guttenberg Book Data:\n"; 
 	cout << "\n\tTitle: "  << gbook.getTitle();
 	cout << "\n\tId: "  << gbook.getId();
 
-	cout << "\tAuthors:\n";
+	cout << "\n\tAuthors:\n";
 	for (auto& k: gbook.getAuthors()) 
 		cout << "\t\t" << k << ",";
 	cout << "\n\tDate Added: "  << gbook.getDateAdded();
-	cout << "\n\tLanguage: ";
-	for (auto& i : gbook.getLang())
-		cout << "\t\t" << i << ",";
+	cout << "\n\tLanguage: " << gbook.getLang();
 	cout << "\n\tGenres: \n\t\t";
 	for (auto& i : gbook.getGenres())
 		cout << i << ",";
+	cout << "\n\n";
 
-	cout << "testing search function.."  << endl;
+	cout << "Testing search function..Search for Pride and Prejudice, title category"  << endl;
 	vector<GutenbergBook> gbook_list = ds.searchGutenbergBookData("Pride and Prejudie", "title");
 
-	for (auto & b : gbook_list) {
+	for (auto& b : gbook_list) {
 		cout << b.getId() << "\n";
+		cout << "\n\tAuthors:\n";
+		for (auto& k: gbook.getAuthors()) 
+			cout << "\t\t" << k << ",";
+		cout << "\n\tTitle: "  << b.getTitle();
+		cout << "\n\tId: "  << b.getId();
 	}
+	cout << "\n";
 	
 
 
