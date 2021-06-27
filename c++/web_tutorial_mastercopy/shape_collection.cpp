1523f822e7707f13ae98ab08f5549da208ee25ee
Kalpathi Subramanian <krs@uncc.edu>
2021-06-27 19:38:20 -0400

    shape collection test updates

--- c++/web_tutorial_mastercopy/shape_collection.cpp
+++ c++/web_tutorial_mastercopy/shape_collection.cpp
@@ -1,10 +1,11 @@
 #include "Bridges.h"
 #include "SymbolCollection.h"
+#include "SymbolGroup.h"
 #include "Rectangle.h"
 #include "Polyline.h"
 #include "Polygon.h"
 #include "Circle.h"
-#include "Label.h"
+#include "Text.h"
 
 using namespace bridges;
 
@@ -32,24 +33,34 @@ int main(int argc, char **argv) {
 	SymbolCollection sc;
 
 	// draw a rectangle
-	Rectangle s1(-25, 0, 50, 50);
+	Rectangle s1(-50, 0, 25, 25);
 	s1.setFillColor(Color("red"));
-	s1.setOpacity(0.3f);
-	sc.addSymbol(&s1);
-	
+	s1.setOpacity(0.1f);
+//	sc.addSymbol(&s1);
 
+	
 	// and a circle next to it
-	Circle s2(25, 0, 25);
+	Circle s2(50, 0, 25);
 	s2.setFillColor(Color("green"));
-	sc.addSymbol(&s2);
+	s2.setOpacity(1.0f);
+//	sc.addSymbol(&s2);
 
 	// draw a triangle above
+	SymbolGroup g;
+		g.translate(3., 10.);
+		g.addSymbol(&s1);
+		g.addSymbol(&s2);
+
+	SymbolCollection sc2;
+		sc2.addSymbol (&g);
+
 	Polygon s3;
 	s3.addPoint(-25, -10);
 	s3.addPoint(25, -10);
 	s3.addPoint(0, 50);
 	s3.setStrokeColor("darkorchid");
 	s3.setStrokeWidth(2.0f);
+	s3.setOpacity(1.0f);
 	sc.addSymbol(&s3);
 
 	// draw axes
@@ -58,12 +69,14 @@ int main(int argc, char **argv) {
 	s4.addPoint(100, 0);
 	s4.setStrokeColor("magenta");
 	s4.setStrokeWidth(2.0f);
+	s4.setOpacity(1.0f);
 	sc.addSymbol(&s4);
 
 	Polyline s5;
 	s5.addPoint(0, -100);
 	s5.addPoint(0, 100);
 	s5.setStrokeWidth(2.0f);
+	s5.setOpacity(1.0f);
 	sc.addSymbol(&s5);
 
 	Polygon s6;
@@ -73,18 +86,23 @@ int main(int argc, char **argv) {
 	s6.setStrokeColor("cyan");
 	s6.setFillColor("yellow");
 	s6.setStrokeWidth(3.0f);
+	s6.setOpacity(1.0f);
 	sc.addSymbol(&s6);
 
 
 
-	Label l;
-	l.setLocation(0, 25);
-	l.setFontSize(12);
+	Text l;
+	float loc[2] = {0, 25};
+	l.setAnchorLocation(loc);
+	l.setFontSize(50.0f);
 	l.setStrokeWidth(1.0f);
 	l.setLabel("test label");
 	l.setStrokeColor("purple");
 	l.setRotationAngle(90.);
+	l.setOpacity(1.0f);
 	sc.addSymbol(&l);
+
+	sc.addSymbol(&g);
 
 	// set visualizer type
 	bridges.setDataStructure(sc);
