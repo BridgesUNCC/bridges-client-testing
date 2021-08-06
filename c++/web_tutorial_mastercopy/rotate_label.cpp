ec5da99d0aac66bc163f358fee60d7bcbd9b81ee
Kalpathi Subramanian <krs@uncc.edu>
2021-08-06 09:51:41 -0400

    broken rotate label test

--- c++/web_tutorial_mastercopy/rotate_label.cpp
+++ c++/web_tutorial_mastercopy/rotate_label.cpp
@@ -1,7 +1,7 @@
 #include "Bridges.h"
 #include "SymbolCollection.h"
 #include "Rectangle.h"
-#include "Label.h"
+#include "Text.h"
 #include "Circle.h"
 
 #include <vector>
@@ -34,61 +34,61 @@ int main(int argc, char **argv) {
 
 	
 
-	Label l;
-	l.setLocation(0, 0);
+	Text l;
+	l.setAnchorLocation(0, 0);
 	l.setFontSize(8.);
 	l.setStrokeWidth(0.0f);
-	l.setLabel("abcdljjjlllmmmm");
+	l.setText("abcdljjjlllmmmm");
 	l.setStrokeColor("purple");
-	l.setRotationAngle(15.);
-	sc.addSymbol(&l);
+//	l.setRotationAngle(15.);
+	sc.addSymbol(l);
 
 	// draw a rectangle
 
-	Label l2;
-	l2.setLocation(50, 50);
+	Text l2;
+	l2.setAnchorLocation(50, 50);
 	l2.setFontSize(12.);
 	l2.setStrokeWidth(0.0f);
-	l2.setLabel("ABCDLJJJIIILLLMMMM");
+	l2.setText("ABCDLJJJIIILLLMMMM");
 	l2.setStrokeWidth(0.0f);
 	l2.setFillColor("purple");
-	l2.setRotationAngle(-50.0f);
-	sc.addSymbol(&l2);
+//	l2.setRotationAngle(-50.0f);
+	sc.addSymbol(l2);
 	
 	vector<float> bbox =  l.getBoundingBox();
 	Rectangle r1(bbox[0], bbox[1], bbox[2]-bbox[0], bbox[3]- bbox[1]);
 	r1.setFillColor("red");
 	r1.setOpacity(0.1f);
-	sc.addSymbol(&r1);
+	sc.addSymbol(r1);
 
 	bbox =  l2.getBoundingBox();
 	Rectangle r2(bbox[0], bbox[1], bbox[2]-bbox[0], bbox[3]- bbox[1]);
 	r2.setFillColor("green");
 	r2.setOpacity(0.3f);
-	sc.addSymbol(&r2);
+	sc.addSymbol(r2);
 
 	// draw a small circle
 	Circle c1 (0,0, 3);
 	c1.setFillColor(Color("green"));
 	c1.setOpacity(0.5f);
-	sc.addSymbol(&c1);
+	sc.addSymbol(c1);
 
 
-	Label l3;
-	l3.setLocation(-50, -20);
+	Text l3;
+	l3.setAnchorLocation(-50, -20);
 	l3.setFontSize(12.);
 	l3.setStrokeWidth(0.0f);
-	l3.setLabel("ZYX cba TUV def CBA");
+	l3.setText("ZYX cba TUV def CBA");
 	l3.setStrokeWidth(0.0f);
 	l3.setFillColor("purple");
-	l3.setRotationAngle(-90.0f);
-	sc.addSymbol(&l3);
+//	l3.setRotationAngle(-90.0f);
+	sc.addSymbol(l3);
 
 	bbox =  l3.getBoundingBox();
 	Rectangle r3(bbox[0], bbox[1], bbox[2]-bbox[0], bbox[3]- bbox[1]);
 	r3.setFillColor("red");
 	r3.setOpacity(0.3f);
-	sc.addSymbol(&r3);
+	sc.addSymbol(r3);
 	// set visualizer type
 	bridges.setDataStructure(sc);
 
