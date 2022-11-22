1a3f9810c717779fb13955918a18f9a030e5b4e3
Zack Clements <zclement@uncc.edu>
2022-11-22 14:25:50 -0500

    Updated 3d_game_test WIP

--- c++/web_tutorial_mastercopy/3d_game_test.cpp
+++ c++/web_tutorial_mastercopy/3d_game_test.cpp
@@ -2,6 +2,7 @@
 #include "DataSource.h"
 #include "TerrainMesh.h"
 #include "data_src/ElevationData.h"
+#include "NonBlockingGame3D.h"
 
 #include "Scene.h"
 #include "Camera.h"
@@ -12,78 +13,45 @@
 
 using namespace std;
 using namespace bridges;
+using namespace bridges::game;
 
-int main(int argc, char **argv) {
+struct test: public NonBlockingGame3D {
+    int frame;
+    bool flag;
+    double iTime;
+    
+    test(int assID, string username, string apikey): NonBlockingGame3D(0, username, apikey){
+        frame = 0;
+        flag = true;
+        iTime = 0.0;
+    }
+    
+    virtual void initialize() override {
+        DataSource ds;
+        dataset::ElevationData elev_data;
+        elev_data = ds.getElevationData(33.394759, -122.299805, 42.747012,
+                                        -114.916992, 0.2);
+        vector<float> verts;
+        for (auto k : elev_data.getData())
+                verts.push_back (k);
+        TerrainMesh terrain('terr', elev_data.getRows(), elev_data.getCols(), verts);
+        float position[] = {0., 0., 0.};
+        scene.add(terrain);
+    }
+    
+    virtual void gameLoop() override {
+        iTime += 1;
+        for(int i = 0; i < scene.get('terr').getRows(); i++){
+            for(int j = 0; j < scene.get('terr').getCols(); j++){
+                scene.get('terr').vertices[i][j] = sin(iTime + i + j) * 100;
+                scene.get('terr').colors[i][j] = [sin(iTime + i), sin(iTime + i+ j), sin(iTime + j)]
+            }
+        }
+    }
+};
 
-	// create Bridges object
-#if TESTING
-	// command line args provide credentials and server to test on
-	Bridges bridges (atoi(argv[1]), argv[2], argv[3]);
-
-	if (argc > 4)
-		bridges.setServer(argv[4]);
-
-	bridges.setTitle("3D Game Engine Test");
-#else
-	Bridges bridges (YOUR_ASSSIGNMENT_NUMBER, "YOUR_USER_ID", "YOUR_API_KEY");
-#endif
-
-	bridges.setServer("clone");
-
-	//create the Bridges object, set credentials
-
-	DataSource ds;
-	dataset::ElevationData elev_data;
-
-	// Get a List of Elevation data  records from Bridges
-	try {
-		elev_data = ds.getElevationData(33.394759, -122.299805, 42.747012, 
-										-114.916992, 0.2);
-	}
-	catch (std::string s) {
-		std::cerr << "Exception: " << s << "\n";
-	}
-
-	// create a terrain mesh
-	
-	vector<float> verts;
-	for (auto k : elev_data.getData()) 
-			verts.push_back (k);
-//	TerrainMesh terrain(elev_data.getRows(), elev_data.getCols(), verts); 
-	TerrainMesh terrain(5, 5, verts); 
-
-
-
-
-	// specify scene
-	float position[] = {0., 0., 0.};
-	Scene sc("terrain scene", 90, position);
-		sc.add(terrain);
-
-	bridges.setDataStructure (&sc);
-
-	// to print JSON from bridges::visualize() uncomment the following 2 lines
-	// but it might crash/burn since the JSON should not be sent to the server
-	// as it needs to use the game API; this was done just for testing the JSON
-
-	// bridges.setJSONFlag(true);
-	// bridges.visualize();
-
-	// get JSON
-	const string scene_json = sc.getDataStructureRepresentation();
-	cout << "scene JSON:  " << scene_json << endl;
-
-	// print the data dimensions  and the firs 10 elevation data values
-	cout << "\tWidth: " << elev_data.getCols() << endl
-		<< "\tHeight: " << elev_data.getRows() << endl
-		<< "\tCell Size: " << elev_data.getCellSize() << endl
-		<< "\tLower Left Corner: "  << elev_data.getxll()  << 
-				 ", " <<  elev_data.getyll() <<endl;
-
-	cout << "The first 10 elevation values.." << endl;
-	for (int k = 0; k < 10; k++)
-		cout << elev_data.getVal(0, k) << "  " << endl;
-	cout << endl;
-
-	return 0;
+int main() {
+    test game(600, "USERNAME", "ID");
+    game.start();
 }
+
