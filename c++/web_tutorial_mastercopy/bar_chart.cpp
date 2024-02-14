0f80b989f33b6bc7fdd423a358cbe673da7533a1
Matthew McQuaigue <mmcquaig@uncc.edu>
2024-02-13 19:41:08 -0500

    Update bar_chart test

--- c++/web_tutorial_mastercopy/bar_chart.cpp
+++ c++/web_tutorial_mastercopy/bar_chart.cpp
@@ -4,10 +4,15 @@
  */
 
 #include "Bridges.h"
+#include "DataSource.h"
 #include "BarChart.h"
 
-using namespace bridges;
+#include <iostream>
+#include <string>
+#include <vector>
+
 using namespace std;
+using namespace bridges;
 
 
 int main (int argc, char **argv) {
@@ -18,30 +23,29 @@ int main (int argc, char **argv) {
 
 	if (argc > 4)
 		bridges.setServer(argv[4]);
-
-
 #else
 	Bridges bridges (YOUR_ASSSIGNMENT_NUMBER, "YOUR_USER_ID", "YOUR_API_KEY");
 #endif
-	// set title
-	bridges.setTitle("A Bar Chart Example");
 
-	// set  description
-	bridges.setDescription("Demonstrates how to  draw a  bar chart using BRIDGES");
+	BarChart bar;
+	bar.setTitle("Hello World BarChart!");
+	bar.setSubTitle("Woah!");
+	bar.setBinsLabel("Country");
+	bar.setSeriesLabel("Population");
+	bar.setBarAlignment("horizontal");
+	bar.setTooltipSuffix(" millions");
 
-	// create the linked list elements with student names
-	BarChart bc;
-	
-	bc.setXLabel ("US States");
-	bc.setYLabel ("Population");
-	
-	vector<double> pop_data{10550000., 19840000., 39240000., 29530000., 10960000.};
-	vector<string> bins {"North Carolina", "New York", "California", "Texas",
-								"Rhode Island"};
-	bc.setSeriesBins(bins);
+	vector<string> bins{"africa", "america", "Asia", "Europe"};
+	bar.setSeriesBins(bins);
 
-	bc.addDataSeries ("State Populations", pop_data);
+	vector<double> y1{10, 100, 40, 23};
+	vector<double> y2{100, 10, 80, 232};
+	bar.addDataSeries("year 1", y1);
+	bar.addDataSeries("year 2", y2);
 
-	bridges.setDataStructure(&bc);
+	bridges.setDataStructure(bar);
 	bridges.visualize();
+
+
+	return 0;
 }
