958df080ae84ab83b6baaf1e78558044a0d5418b
Kalpathi Subramanian <krs@uncc.edu>
2024-09-15 21:37:22 -0400

    updated bar chart test, not working!

---
+++ java/web_tutorial_mastercopy/barchart.java
@@ -0,0 +1,34 @@
+import bridges.base.BarChart;
+import bridges.connect.Bridges;
+import bridges.validation.RateLimitException;
+
+import java.io.IOException;
+import java.util.*;
+
+public class barchart {
+
+	public static void main(String[] args) throws IOException, RateLimitException, InterruptedException {
+#if TESTING
+
+		Bridges bridges = new Bridges(Integer.parseInt(args[0]), args[1], args[2]);
+		bridges.setServer(args[3]);
+
+		bridges.setTitle("Bar Chart Test");
+		bridges.setDescription("Bar Chart Test in Java");
+
+#else
+		Bridges bridges = new Bridges(YOUR_ASSIGNMENT_NUMBER, "YOUR_USER_ID",
+			"YOUR_API_KEY");
+#endif
+
+		BarChart bar = new BarChart();
+		bar.setTitle("Bar Chart Example");
+
+		double y1[] = new double[] {10, 100, 40, 23};
+		double y2[] = new double[] {100, 10, 80, 232};
+		bar.addDataSeries("year 1", y1);
+		bar.addDataSeries("year 2", y2);
+		bridges.setDataStructure(bar);
+		bridges.visualize();
+	}
+}
