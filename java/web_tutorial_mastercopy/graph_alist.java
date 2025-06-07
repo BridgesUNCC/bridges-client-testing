4c0469af0f7e2c11bc6d890101bfd206d0226add
Kalpathi Subramanian <krs@charlotte.edu>
2025-06-07 18:08:36 -0400

    updated graph test

--- java/web_tutorial_mastercopy/graph_alist.java
+++ java/web_tutorial_mastercopy/graph_alist.java
@@ -104,8 +104,6 @@ public class graph_alist {
 
 		// Pass the graph object to BRIDGES
 		bridges.setDataStructure(g);
-bridges.setLabelFlag(true);
-bridges.setJSONFlag(true);
 
 		// Finaly we call the visualize function
 		bridges.visualize();
